9cedd935167f4f8bc46778ed120f73c096f35f63
justpresh12 <113901708+justpresh12@users.noreply.github.com>
2023-03-29 10:53:34 +0100

    First changes

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-let heading = document.querySelector("h1");
+function roll(){
+  let heading = document.querySelector("h1");
 
-let randomNumber1 = Math.ceil(Math.random() * 6);
-let randomNumber2 = Math.ceil(Math.random() * 6);
+  let randomNumber1 = Math.ceil(Math.random() * 6);
+  let randomNumber2 = Math.ceil(Math.random() * 6);
 
-let imageSrc1 = "images/dice" + randomNumber1 + ".png";
-let imageSrc2 = "images/dice" + randomNumber2 + ".png";
+  let imageSrc1 = "images/dice" + randomNumber1 + ".png";
+  let imageSrc2 = "images/dice" + randomNumber2 + ".png";
 
-document.querySelector(".img1").setAttribute("src", imageSrc1);
+  document.querySelector(".img1").setAttribute("src", imageSrc1);
 
-document.querySelector(".img2").setAttribute("src", imageSrc2);
+  document.querySelector(".img2").setAttribute("src", imageSrc2);
 
-if(randomNumber1>randomNumber2){
-  heading.innerHTML = "Player 1 wins"; 
-}
-else if(randomNumber1<randomNumber2){
-  heading.innerHTML = "Player 2 wins"; 
-}
-else{
-  heading.innerHTML = "Draw!";
+  if(randomNumber1>randomNumber2){
+    heading.innerHTML = "Player 1 wins"; 
+  }
+  else if(randomNumber1<randomNumber2){
+    heading.innerHTML = "Player 2 wins"; 
+  }
+  else{
+    heading.innerHTML = "Draw!";
+  }
 }
